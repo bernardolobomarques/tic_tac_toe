be4da5db74b7a623179939e0d6ab1548bdc9d143
Bernardo Lobo Marques <161499154+bernardolobomarques@users.noreply.github.com>
2024-12-27 01:27:58 -0300

    Complete

--- main.py
+++ main.py
@@ -1,161 +1,124 @@
 import pygame
 import time
+import sys
 
-COLOR = (50,100, 250)
+COLOR = (50, 100, 250)
 WIDTH = 600
-LENGH = WIDTH
+LENGTH = WIDTH
 FONT = "Courier New"
 LINE_ONE = WIDTH / 3
 LINE_TWO = LINE_ONE * 2
 
 is_player_cross = True
-spaces = []
+spaces = [False] * 9
 
-class Screen():
+class Screen:
     def __init__(self) -> None:
-        self.display = pygame.display.set_mode((WIDTH, LENGH))
+        self.display = pygame.display.set_mode((WIDTH, LENGTH))
         pygame.display.set_caption("Tic Tac Toe")
-        self.fonte = FONT
+        self.font = FONT
         self.display.fill(COLOR)
 
     def draw(self):
-        pygame.draw.line(self.display, "white", (0, LINE_ONE), (WIDTH,LINE_ONE), 2) #FIRST HORIZONTAL LINE
-        pygame.draw.line(self.display, "white", (0, LINE_TWO), (WIDTH,LINE_TWO), 2) #SECOND HORIZONTAL LINE
-
-        pygame.draw.line(self.display, "white", (LINE_ONE, 0), (LINE_ONE,WIDTH), 2) #FIRST HORIZONTAL LINE
-        pygame.draw.line(self.display, "white", (LINE_TWO, 0), (LINE_TWO,WIDTH), 2) #SECOND HORIZONTAL LINE
-
+        for i in range(1, 3):
+            pygame.draw.line(self.display, "white", (0, i * LINE_ONE), (WIDTH, i * LINE_ONE), 2)
+            pygame.draw.line(self.display, "white", (i * LINE_ONE, 0), (i * LINE_ONE, WIDTH), 2)
         pygame.display.update()
 
     @staticmethod
     def click_tracking(pos):
         x, y = pos
+        row = y // 200
+        col = x // 200
+        box_num = row * 3 + col + 1
+        center_x = col * 200 + 100
+        center_y = row * 200 + 100
+        return box_num, center_x, center_y
 
-        if x <= 200:
-            #Box 1
-            if y <= 200:
-                return 1, 100, 100
-            
-            #Box 4
-            elif y > 200 and y <= 400:
-                return 4, 100, 300
-            
-            #Box 7
-            elif y > 400 and y <= 600:
-                return 7, 100, 500
-
-        elif x > 200 and x <= 400:
-            #Box 2
-            if y <= 200:
-                return 2, 300, 100
-            
-            #Box 5
-            elif y  > 200 and y <= 400:
-                return 5, 300, 300
-            
-            #Box 7
-            elif y > 400 and y <= 600:
-                return 8, 300, 500
-            
-        elif x > 400 and x <= 600:
-            #Box 3
-            if y <= 200:
-                return 3, 500, 100
-            
-            #Box 6
-            elif y > 200 and y <= 400:
-                return 6, 500, 300
-            
-            #Box 9
-            elif y > 400 and y <=600:
-                return 9, 500, 500
-            
     def winner(self, s):
-        self.display.fill("black")
-        font = pygame.font.SysFont(FONT, 42)
-        text = font.render(f"{s} won!", True, "white")
-        text_rect = text.get_rect(center=(WIDTH//2, LENGH//2))
-        self.display.blit(text, text_rect)
+        self.display_message(f"{s} won!")
 
-     
+    def draw_game(self):
+        self.display_message("It's a draw!")
+
+    def display_message(self, message):
+        winner_screen = pygame.display.set_mode((WIDTH, LENGTH))
+        winner_screen.fill("black")
+        font = pygame.font.SysFont(FONT, 42)
+        text = font.render(message, True, "white")
+        text_rect = text.get_rect(center=(WIDTH // 2, LENGTH // 2 - 50))
+        winner_screen.blit(text, text_rect)
+
+        button_font = pygame.font.SysFont(FONT, 30)
+        button_text = button_font.render("Play Again", True, "black")
+        button_rect = pygame.Rect(WIDTH // 2 - 100, LENGTH // 2, 200, 50)
+        pygame.draw.rect(winner_screen, "white", button_rect)
+        button_text_rect = button_text.get_rect(center=button_rect.center)
+        winner_screen.blit(button_text, button_text_rect)
+
+        pygame.display.update()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    if button_rect.collidepoint(event.pos):
+                        return
+
 def draw_symbol(box, screen):
     n, x, y = box
     font = pygame.font.SysFont(FONT, 60)
     global is_player_cross
-    
-    if is_player_cross:
-        text = font.render("X", True, "white")
-    else:
-        text = font.render("O", True, "white")
+
+    text = font.render("X" if is_player_cross else "O", True, "white")
     text_rect = text.get_rect(center=(x, y))
-
-    if is_player_cross == True:
-        is_player_cross = False
-    else:
-        is_player_cross = True
-
     screen.display.blit(text, text_rect)
+    spaces[n - 1] = 1 if is_player_cross else 2
+    is_player_cross = not is_player_cross
 
 def win(spaces):
-    if spaces[0] == spaces[1] == spaces[2] and spaces[0] != False:
-        return spaces[0]
-    elif spaces[3] == spaces[4] == spaces[5] and spaces[3] != False:
-        return spaces[3]
-    
-    elif spaces[6] == spaces[7] == spaces[8] and spaces[6] != False:
-        return spaces[6]
-    
-    elif spaces[0] == spaces[3] == spaces[6] and spaces[0] != False:
-        return spaces[0]
-    
-    elif spaces[1] == spaces[4] == spaces[7] and spaces[1] != False:
-        return spaces[1]
-    
-    elif spaces[2] == spaces[5] == spaces[8] and spaces[2] != False:
-        return spaces[2]
-    
-    elif spaces[0] == spaces[4] == spaces[8] and spaces[0] != False:
-        return spaces[0]
-    
-    elif spaces[2] == spaces[4] == spaces[6]  and spaces[2] != False:
-        return spaces[2]
-    else:
-        return False
+    win_conditions = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Horizontal
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Vertical
+        (0, 4, 8), (2, 4, 6)              # Diagonal
+    ]
+    for a, b, c in win_conditions:
+        if spaces[a] == spaces[b] == spaces[c] and spaces[a] != False:
+            return spaces[a]
+    return False
 
 def loop():
     screen = Screen()
     is_playing = True
     global spaces
-    spaces = [False, False, False, False, False, False, False, False, False]
+    spaces = [False] * 9
 
     while is_playing:
         screen.draw()
         control = win(spaces)
         if control == False:
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    return
-                
-                #Posicionamento do mouse
-                if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                    pos = pygame.mouse.get_pos()
-                    box = screen.click_tracking(pos)
-                    
-                    if not spaces[box[0]-1]:
-                        draw_symbol(box, screen)
-                        if is_player_cross:
-                            spaces[box[0]-1] = 2
-                        else:
-                            spaces[box[0]-1] = 1
+            if all(spaces):
+                screen.draw_game()
+                spaces = [False] * 9
+                is_player_cross = True
+                screen.display.fill(COLOR)
+            else:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        return
+                    if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                        pos = pygame.mouse.get_pos()
+                        box = screen.click_tracking(pos)
+                        if not spaces[box[0] - 1]:
+                            draw_symbol(box, screen)
         else:
-            for _ in range(10):
-                screen.winner(control)
-                time.sleep(5)
-            break
-
+            screen.winner("Player X" if control == 1 else "Player O")
+            spaces = [False] * 9
+            is_player_cross = True
+            screen.display.fill(COLOR)
         pygame.time.Clock().tick(60)
-
 
 def start_game():
     pygame.init()
